5fa489b4889519a93de7989f651d6ff644f61f0a
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-10-20 04:50:56 +0600

    Add Products to database functionality added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sml8dnv.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sml8dnv.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,11 +23,14 @@ async function run() {
     await client.connect();
 
     const database = client.db("brandshopDB");
-    const brandCollection = database.collection("brands");
+    const userCollection = database.collection("users");
     const productCollection = database.collection("products");
 
-    app.get("/", async (req, res) => {
-        res.send("Server running")
+    app.post("/addproduct", async (req, res) => {
+        const product = req.body;
+        console.log(product);
+        const result = await productCollection.insertOne(product);
+        res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -35,7 +38,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
